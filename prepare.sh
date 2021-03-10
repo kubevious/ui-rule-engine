e68e773b3e88610bf46ac45185569217b2dff401
Ruben Hakopian <ruben.hakopian@gmail.com>
2021-03-10 13:26:44 -0800

    dependency update.

--- prepare.sh
+++ prepare.sh
@@ -7,4 +7,6 @@ rm -rf node_modules/
 rm -rf dist/
 
 npm install
-npm update the-lodash the-promise @kubevious/ui-framework @kubevious/ui-middleware @kubevious/ui-components
+npm update the-lodash @kubevious/ui-framework @kubevious/ui-middleware @kubevious/ui-components
+
+${MY_DIR}/build.sh
