48a1a9d5c8ead02ea9712bfa544f7dc83fda249b
Semyon Pukhov <semyonpuchof@gmail.com>
2021-05-14 18:19:50 +0300

    Fix MarkerPreview export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { RuleEditor } from './RuleEditor';
 
 export { MarkerEditor } from './MarkerEditor';
 
-export { MarkerPreview } from './MarkerPreview';
+export * from './MarkerPreview';
 
 export * from './hooks/useMarkerEditorActions';
 export * from './hooks/useRuleEditorActions';
